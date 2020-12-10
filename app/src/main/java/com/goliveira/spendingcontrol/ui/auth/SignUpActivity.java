97b9908c39b01da323ba1c1490e597e3dfa4e30e
GlaucioOliveira <glaucio.dev@gmail.com>
2020-12-10 17:16:38 -0300

    adding some refactoring, and some visual fixes

--- app/src/main/java/com/goliveira/spendingcontrol/ui/auth/SignUpActivity.java
+++ app/src/main/java/com/goliveira/spendingcontrol/ui/auth/SignUpActivity.java
@@ -33,85 +33,121 @@ public class SignUpActivity extends AppCompatActivity {
     CheckBox agreementCheckBox;
     TextView errorView;
 
+    private void ToastError(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sign_up);
 
         mAuth = FirebaseAuth.getInstance();
 
+        LoadFragmentViews();
+
+        LoadFragmentButtonListeners();
+    }
+
+    private boolean ValidateActivity() {
+        if (signUpEmailTextInput.getText().toString().contentEquals("")) {
+            ToastError("Email can't be empty");
+            return false;
+        }
+
+        if (signUpPasswordTextInput.getText().toString().contentEquals("")) {
+            ToastError("Password can't be empty");
+            return false;
+        }
+
+        if (agreementCheckBox.isChecked()) {
+            ToastError("Please agree to terms and the Condition!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void LoadFragmentViews() {
         signUpEmailTextInput = findViewById(R.id.signUpEmailTextInput);
         signUpPasswordTextInput = findViewById(R.id.signUpPasswordTextInput);
         signUpButton = findViewById(R.id.signUpButton);
         agreementCheckBox = findViewById(R.id.agreementCheckbox);
         errorView = findViewById(R.id.signUpErrorView);
+    }
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
+    private void FireBaseSignUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                if (signUpEmailTextInput.getText().toString().contentEquals("")) {
-                    errorView.setText("Email cannot be empty");
-                } else if (signUpPasswordTextInput.getText().toString().contentEquals("")) {
-                    errorView.setText("Password cannot be empty");
-                } else if (!agreementCheckBox.isChecked()) {
-                    errorView.setText("Please agree to terms and Condition");
-                } else {
-                    mAuth.createUserWithEmailAndPassword(signUpEmailTextInput.getText().toString(), signUpPasswordTextInput.getText().toString()).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                try {
-                                    if (user != null)
-                                        user.sendEmailVerification()
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()) {
-                                                    Log.d(TAG, "Email sent.");
-                                                    User newUser = new User(user.getUid(),user.getEmail());
-                                                    newUser.save();
-                                                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                                                            SignUpActivity.this);
-                                                    // set title
-                                                    alertDialogBuilder.setTitle("Please Verify Your EmailID");
-                                                    // set dialog message
-                                                    alertDialogBuilder
-                                                    .setMessage("A verification Email Is Sent To Your Registered EmailID, please click on the link and Sign in again!")
-                                                    .setCancelable(false)
-                                                    .setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int id) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    try {
+                        if (user != null)
+                            user.sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d(TAG, "Email sent.");
+                                                User newUser = new User(user.getUid(), user.getEmail());
+                                                newUser.save();
+                                                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                                                        SignUpActivity.this);
+                                                // set title
+                                                alertDialogBuilder.setTitle("Please Verify Your EmailID");
+                                                // set dialog message
+                                                alertDialogBuilder
+                                                        .setMessage("A verification Email Is Sent To Your Registered EmailID, please click on the link and Sign in again!")
+                                                        .setCancelable(false)
+                                                        .setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+                                                            public void onClick(DialogInterface dialog, int id) {
 
-                                                            Intent signInIntent = new Intent(SignUpActivity.this, SignInActivity.class);
-                                                            SignUpActivity.this.finish();
-                                                        }
-                                                    });
-                                                    // create alert dialog
-                                                    AlertDialog alertDialog = alertDialogBuilder.create();
-                                                    // show it
-                                                    alertDialog.show();
+                                                                Intent signInIntent = new Intent(SignUpActivity.this, SignInActivity.class);
+                                                                SignUpActivity.this.finish();
+                                                            }
+                                                        });
+                                                // create alert dialog
+                                                AlertDialog alertDialog = alertDialogBuilder.create();
+                                                // show it
+                                                alertDialog.show();
 
-                                                }
                                             }
-                                        });
-                                } catch (Exception e) {
-                                    errorView.setText(e.getMessage());
-                                }
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                                if (task.getException() != null) {
-                                    errorView.setText(task.getException().getMessage());
-                                }
-                            }
-                        }
-                    });
+                                        }
+                                    });
+                    } catch (Exception e) {
+                        errorView.setText(e.getMessage());
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    if (task.getException() != null) {
+                        errorView.setText(task.getException().getMessage());
+                    }
                 }
             }
         });
+
     }
+
+    private void LoadFragmentButtonListeners() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ValidateActivity() == false) return;
+
+                String email = signUpEmailTextInput.getText().toString();
+                String password = signUpPasswordTextInput.getText().toString();
+
+                FireBaseSignUp(email, password);
+            }
+        });
+    }
+
 }
